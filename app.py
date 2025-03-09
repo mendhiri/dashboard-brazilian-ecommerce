7cfa13826c117c84e557ac851c252ca6e03f0b54
mendhiri <mendhiri.keenansa@gmail.com>
2025-03-09 10:25:14 +0700

    update-3: final notebook

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ products_df = pd.read_csv("./E-Commerce Public Dataset/products_dataset.csv")
 sellers_df = pd.read_csv("./E-Commerce Public Dataset/sellers_dataset.csv")
 order_reviews = pd.read_csv("./E-Commerce Public Dataset/order_reviews_dataset.csv")
 
+# Load the dataset 
+big_orders = orders_df.merge(orders_payment_df, on='order_id', how='left')\
+             .merge(order_reviews, on='order_id', how='left').merge(cust_df[['customer_id','customer_city', 'customer_state']], on='customer_id', how='left').dropna()
+
+big_orders = big_orders[['order_id', 'customer_id', 'order_status', 'order_purchase_timestamp',	'order_approved_at',
+            'order_delivered_carrier_date',	'order_delivered_customer_date',	'order_estimated_delivery_date',
+            'payment_type','payment_value', 'review_score', 'customer_city', 'customer_state']]
+
 # Load external CSS
 def load_css(file_name):
     with open(file_name) as f:
@@ -23,26 +31,53 @@ def load_css(file_name):
 # Apply CSS from an external file
 load_css("style.css")
 
-# Streamlit layout with sidebar styling
-st.sidebar.header("Navigation")
-st.sidebar.markdown("<div class='sidebar-container'>", unsafe_allow_html=True)
-page = st.sidebar.radio("Go to", ["Home", "About"])
-st.sidebar.markdown("</div>", unsafe_allow_html=True)
 
-if page == "Home":
-    st.markdown("<p class='main-title'>Welcome to the Styled Streamlit App!</p>", unsafe_allow_html=True)
-    st.write("This is a simple example of a Streamlit app with custom CSS styling.")
-    if st.button("Click Me"):
-        st.success("Button Clicked!")
-    with st.container():
-        st.write("Inside the container")
-        
-        x = np.random.normal(15, 5, 250)
+st.markdown("</div>", unsafe_allow_html=True) 
+
+with st.sidebar:
+
+    st.markdown("<h1> Olist Brazilian Dashboard</h1>", unsafe_allow_html=True)
+
+    card_html = f"""
+    <a href="#" class="card">
+        <h5 class="card-title">Noteworthy Technology Acquisitions 2021</h5>
+        <p class="card-text">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </a>
+    """
+
+    st.markdown(card_html, unsafe_allow_html=True)
     
-        fig, ax = plt.subplots()
-        ax.hist(x=x, bins=15)
-        st.pyplot(fig) 
+    values = st.slider(
+        label='Select a range of values',
+        min_value=0, max_value=100, value=(0, 100)
+    )
+    st.write('Values:', values)
 
-elif page == "About":
-    st.markdown("<p class='main-title'>About This App</p>", unsafe_allow_html=True)
-    st.write("This app demonstrates how to use CSS in Streamlit.")
+with st.container():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Kolom 1")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+    
+    with col2:
+        st.header("Kolom 2")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+    
+    with col3:
+        st.header("Kolom 3")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+
+with st.container():
+    st.write("Inside the container")
+    
+    x = np.random.normal(15, 5, 250)
+ 
+    fig, ax = plt.subplots()
+    ax.hist(x=x, bins=15)
+    st.pyplot(fig) 
+ 
+
+st.markdown("<p class='main-title'>Welcome to the Styled Streamlit App!</p>", unsafe_allow_html=True)
+st.write("This is a simple example of a Streamlit app with custom CSS styling.")
+if st.button("Click Me"):
+    st.success("Button Clicked!")
